790f552d5cc90f71571a929e5d42ef50f1e7b17f
Pashkov Stanislav <eshkaStas@rambler.ru>
2023-12-17 21:15:28 +0300

    Main changed

--- src/main/kotlin/DifferentFees.kt
+++ src/main/kotlin/DifferentFees.kt
@@ -1,23 +1,30 @@
+import kotlin.math.max
+
+const val ERROR_TYPE = -1
+const val ERROR_LIMIT = -2
+//const val MAX_TRANSFER_AMOUNT_VMMM = 150_000
+//const val MAX_MONTHLY_TRANSFER_VMMM = 600_000
+//const val MAX_TRANSFER_AMOUNT_VK = 15_000
+//const val MAX_MONTHLY_TRANSFER_VK = 40_000
+
 fun main() {
-    var amount = commissionCalculate("MasterCard", amount = 75000)
-    amount = commissionCalculate("Maestro", amount = 75001)
-    amount = commissionCalculate("MasterCard", 50000, amount = 25000)
-    amount = commissionCalculate("Maestro", 40000, amount = 35001)
-    amount = commissionCalculate(amount = 75001)
-    amount = commissionCalculate(monthlyTransfers = 50000, amount = 30000)
+    println(commissionCalculate(cardType = "MasterCard", amount = 1000000))
+
 }
 
-fun commissionCalculate(cardType: String = "VK Pay", monthlyTransfers: Int = 0, amount: Int) {
-    val commissionStart = when {
-        monthlyTransfers > 75000 -> 0.0075 * amount
-        monthlyTransfers + amount > 75000 -> 0.0075 * amount
-        else -> 0.0
+fun commissionCalculate(cardType: String = "VK Pay", monthlyTransfers: Int = 0, amount: Int) =
+    when (cardType) {
+        "Visa", "Mir" -> if (amount <= 150_000 && amount + monthlyTransfers <= 600_000)
+            max(35, (amount * 0.0075).toInt()) else ERROR_LIMIT
+
+
+        "Maestro", "MasterCard" -> if (amount <= 150_000 && amount + monthlyTransfers <= 600_000)
+            if (amount > 75000) (0.006 * amount + 20).toInt() else 0
+        else ERROR_LIMIT
+
+
+        "VK Pay" -> if (amount <= 15_000 && amount + monthlyTransfers <= 40_000) 0 else ERROR_LIMIT
+        else -> ERROR_TYPE
+
     }
-    val card = when (cardType) {
-        "Maestro", "MasterCard" -> commissionStart
-        else -> 0.0
-    }
-    val commission = String.format("%.2f", card)
-    println("Комиссия: $commission копеек ")
-}
 
